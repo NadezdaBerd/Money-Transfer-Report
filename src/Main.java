497967e6756345869ffd32c71264ad19d4f818aa
Nadezda <nbam@mail.rugit config --global user.email  nbam@mail.ru>
2021-04-01 22:30:17 +0300

    open

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int transferredSum = 500_000_000;
+        int total = currentBalance + transferredSum;
+        System.out.println("Total: "+total);
+    }
+}
